6440b2fa8432595304ea0510ba1b58270a44a7d8
lbroglio <lbroglio@iastate.edu>
2023-12-26 17:08:07 -0600

    Renamed stringComparison.py

--- webScraper.py
+++ webScraper.py
@@ -1,27 +1,29 @@
 from bs4 import BeautifulSoup as bsp
 import requests
 
-import stringComparison
+from stringComparison import findJaccardSimilarity 
 
-def scrapeAmazon(itemName):
+
+def scrapeAmazon(searchFor):
     # Build the URL to scrape from. The target page is the page returned when searching for the item from the amazon home page
     # The URL for these searches is https://www.amazon.com/s?k={item name}+{other words of item name}
 
     # Add the item name to the end of the base url with all of its spaces replaced with '+' characters
-    targetURL = "https://www.amazon.com/s?k" + itemName.replace(' ', '+')
+    targetURL = "https://www.amazon.com/s?k" + searchFor.replace(' ', '+')
     
     # Get the HTML page corresponding to the URL
     try:
         targetPage =  requests.get(targetURL)
     except requests.exceptions.RequestException  as errh: 
         print(f"ERROR: Could not reach {targetURL}. Root Cause: {errh}")
-
+    
+    bspPage = bsp.BeatifulSoup(targetPage)
     # Find all of the item elements by targeting all elements of the "a-section" class
     searchItems = bsp.find_all("div", {"class": "a-section"})
 
     # Order the items by the similarity between its name and the user given search parameter
     # This is done by finding treating the names as sets of there words and finding the jaccard similarity
-    orderItems = []
+    orderedItems = []
     for item in searchItems:
         # Get the "h2" tag children of the item
         h2Children = item.findChildren("h2" , recursive=False)
@@ -30,5 +32,10 @@ def scrapeAmazon(itemName):
 
         # Get the text from the found "span" element
         itemName = spanChild.text
+
+        # Get the Jaccard similarity between this items name and the User's search term
+        jSim = findJaccardSimilarity(itemName, searchFor)
+
+        
 
         
